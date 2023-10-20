29466bc055fb9cdd33268bf2d309c9c121d6ee60
Lorena <135393434+lorenaRomao@users.noreply.github.com>
2023-10-20 11:28:49 -0300

    update

---
+++ src/subsistema1/CrmService.java
@@ -0,0 +1,19 @@
+
+packagepackage subsistepackagesubsistepackage;
+
+public class CrmService {
+
+	private CrmService() {
+		super();
+	}
+	
+	public static void gravarCliente(String nome,String treino, String cep, String cidade, String estado) {
+		System.out.println("Cliente salvo no sistema de CRM:");
+		System.out.println("Nome: " + nome);
+		System.out.println("Tipo de Treino: " + treino);
+		System.out.println("CEP: " + cep);
+		System.out.println("Cidade: " +cidade);
+		System.out.println("Estado: " + estado);
+		System.out.println("===================");
+	}
+}
