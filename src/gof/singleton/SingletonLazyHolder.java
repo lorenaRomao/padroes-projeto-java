2d9507f032f1a85629669337505ec35d8cca942e
Lorena <135393434+lorenaRomao@users.noreply.github.com>
2023-10-20 11:40:44 -0300

    Update SingletonLazyHolder.java

--- src/gof/singleton/SingletonLazyHolder.java
+++ src/gof/singleton/SingletonLazyHolder.java
@@ -1,11 +1,9 @@
-package one.digitalinnovation.gof.singleton;
+package gof.singleton;
 
 /**
  * Singleton "Lazy Holder".
  * 
  * @see <a href="https://stackoverflow.com/a/24018148">Referencia</a>
- * 
- * @author falvojr
  */
 public class SingletonLazyHolder {
 
